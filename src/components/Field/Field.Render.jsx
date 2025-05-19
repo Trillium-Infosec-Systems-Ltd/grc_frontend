db0bd0850d68496c97bb2c3c103cb694193f23f3
unknown <muttrafgujjar.787@gmail.com>
2025-05-19 20:57:45 +0500

    Remove duplication and just render field on condition

--- src/components/Field/Field.Render.jsx
+++ src/components/Field/Field.Render.jsx
@@ -6,43 +6,24 @@ import { useMemo } from 'react';
 
 const { TextArea } = Input;
 
-const RenderField = ({ field = {} }) => {
-    const commonProps = {
-        name: field?.fieldname ?? '',
-        label: field?.label ?? '',
-        rules: getValidators(field),
-        hidden: field?.hidden ?? false,
-    };
-
+const RenderField = ({ field = {}, ...rest }) => {
     const formField = useMemo(() => {
-
         switch (field?.fieldtype) {
             case 'Data':
-                return <Form.Item {...commonProps}><Input /></Form.Item>;
+                return <Input {...rest} />;
             case 'Select':
-                return <Form.Item {...commonProps}><GenericSelect field={field} /></Form.Item>;
+                return <GenericSelect {...rest} field={field} />;
             case 'LongText':
-                return <Form.Item {...commonProps}><TextArea rows={4} /></Form.Item>;
+                return <TextArea {...rest} rows={4} />;
             case 'Date':
-                return <Form.Item {...commonProps}><DatePicker style={{ width: '100%' }} /></Form.Item>;
+                return <DatePicker {...rest} style={{ width: '100%' }} />;
             case 'Tags':
-                return <Form.Item {...commonProps}><Select mode="tags" /></Form.Item>;
+                return <Select {...rest} mode="tags" />;
             case 'MultiLink':
-                return <Form.Item {...commonProps}><GenericSelect field={field} mode="multiple" /></Form.Item>;
+            case 'MultiSelect':
+                return <GenericSelect {...rest} field={field} mode="multiple" />;
             case 'Link':
-                return <Form.Item {...commonProps}><GenericSelect field={field} /></Form.Item>;
-            case 'File':
-                return (
-                    <Form.Item
-                        {...commonProps}
-                        valuePropName="fileList"
-                        getValueFromEvent={(e) => Array.isArray(e) ? e : e?.fileList}
-                    >
-                        <Upload beforeUpload={() => false} multiple>
-                            <Button icon={<UploadOutlined />}>Upload File</Button>
-                        </Upload>
-                    </Form.Item>
-                );
+                return <GenericSelect {...rest} field={field} />;
             default:
                 return null;
         }
