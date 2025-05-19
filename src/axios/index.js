97f730552e4c14a660114c29874928c5f6e45de4
unknown <muttrafgujjar.787@gmail.com>
2025-05-19 20:56:46 +0500

    base url export

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,12 +1,12 @@
 import { createAxiosInstance } from './axios.base';
-const BASE_URLS = {
+export const BASE_URLS = {
     auth: 'https://auth.api.com',
     private: 'https://private.api.com',
-    public: 'http://127.0.0.1:8000/api',
+    public: 'http://127.0.0.1:8000/',
     formData: 'https://public.api.com',
 };
 
 export const AuthAPI = createAxiosInstance(BASE_URLS.auth, true);
 export const privateAPI = createAxiosInstance(BASE_URLS.private, true);
-export const PublicAPI = createAxiosInstance(BASE_URLS.public, false);
+export const PublicAPI = createAxiosInstance(BASE_URLS.public + 'api', false);
 export const FormDataAPI = createAxiosInstance(BASE_URLS.formData, true, 'multipart');
