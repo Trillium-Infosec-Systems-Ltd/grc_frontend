e0bffe0fdbd1b4b0b11be9777d05b63df51d1137
unknown <muttrafgujjar.787@gmail.com>
2025-05-30 20:09:01 +0500

    CRUD for Compliance questions

---
+++ src/pages/Administration/Compliance_questions/pages/complianceQuestionsList.jsx
@@ -0,0 +1,49 @@
+import TableBuilder from '../../../../components/Table/Table.Builder';
+import { Button, Tag } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '../../../../constants/routes.constants';
+
+const { COMPLIANCE_QUESTIONS } = ROUTES.PRIVATE.ADMINISTRATION.CHILD;
+
+const ComplianceQuestionsList = () => {
+  const navigate = useNavigate();
+
+  const actions = [
+    {
+      title: 'Details',
+      render: (_, record) => (
+        <Button shape="round" onClick={() => navigate(
+          COMPLIANCE_QUESTIONS.PARENT + COMPLIANCE_QUESTIONS.EDIT,
+          {
+            state: { id: record?.id ?? null },
+          }
+        )}>
+          Manage
+        </Button>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <TableBuilder
+      title='List of Compliance Questions'
+      screen='complaince_question'
+        actionsList={actions}
+        headerLinks={[
+          {
+            Component: null,
+            label: '+ Add New Question',
+            className: 'add-btn',
+            onClick: () =>
+              navigate(
+                COMPLIANCE_QUESTIONS.PARENT + COMPLIANCE_QUESTIONS.CREATE
+              ),
+          },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default ComplianceQuestionsList;
