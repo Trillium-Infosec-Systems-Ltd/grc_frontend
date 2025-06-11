0dca410845f292224feac5420e4f1e6b133bd9a1
unknown <muttrafgujjar.787@gmail.com>
2025-06-11 22:50:21 +0500

    Use Title from typography for heading

--- src/components/Form/FormBuilder.jsx
+++ src/components/Form/FormBuilder.jsx
@@ -1,93 +1,115 @@
-import { useMemo, useState } from 'react';
-import { Form, Button, Row, Col, Upload } from 'antd';
-import useFormHook from '../../hooks/useFormHook';
-import AppLoader from '../Loader/Loader';
-import { KEY } from '../../constants/keysConstants';
-import { buildInitialValues } from './utils';
-import { getValidators } from './validator';
-import { UploadOutlined } from '@ant-design/icons';
-import RenderField from '../Field/FieldRender';
+import { useMemo, useState } from "react";
+import { Form, Button, Row, Col, Upload, Typography } from "antd";
+import useFormHook from "../../hooks/useFormHook";
+import AppLoader from "../Loader/Loader";
+import { KEY } from "../../constants/keysConstants";
+import { buildInitialValues } from "./utils";
+import { getValidators } from "./validator";
+import { UploadOutlined } from "@ant-design/icons";
+import RenderField from "../Field/FieldRender";
 
-const FormBuilder = ({ screen = 'assets', title = '', redirect = '', MODE = KEY.CREATE }) => {
-    const [schema, isLoading, initialData, submit] = useFormHook(screen, MODE);
-    const [fileList, setFileList] = useState([]);
+const { Title } = Typography;
 
-    const [form] = Form.useForm();
+const FormBuilder = ({
+  screen = "assets",
+  title = "",
+  redirect = "",
+  MODE = KEY.CREATE,
+}) => {
+  const [schema, isLoading, initialData, submit] = useFormHook(screen, MODE);
+  const [fileList, setFileList] = useState([]);
 
-    const initialValues = useMemo(() => buildInitialValues(schema, initialData), [schema, initialData]);
+  const [form] = Form.useForm();
 
-    const props = {
-        onRemove: file => {
-            if (fileList.length === 0) return;
-            const index = fileList.indexOf(file);
-            const newFileList = fileList.slice();
-            newFileList.splice(index, 1);
-            setFileList(newFileList);
-        },
-        beforeUpload: file => {
-            setFileList((prevFileList) => [...prevFileList, file]);
-            return false;
-        },
-        // fileList,
-    };
+  const initialValues = useMemo(
+    () => buildInitialValues(schema, initialData),
+    [schema, initialData]
+  );
 
-    const fieldList = useMemo(() => (
-        schema?.fields?.map((field) => {
-            console.log({ field });
+  const props = {
+    onRemove: (file) => {
+      if (fileList.length === 0) return;
+      const index = fileList.indexOf(file);
+      const newFileList = fileList.slice();
+      newFileList.splice(index, 1);
+      setFileList(newFileList);
+    },
+    beforeUpload: (file) => {
+      setFileList((prevFileList) => [...prevFileList, file]);
+      return false;
+    },
+    // fileList,
+  };
 
-            const { hidden = false, fieldname = '', label = '', span = 24, fieldtype } = field;
-            if (!hidden) {
-                const commonProps = {
-                    // ...field,
-                    label,
-                    name: fieldname ?? '',
-                    rules: getValidators(field),
+  const fieldList = useMemo(
+    () =>
+      schema?.fields?.map((field) => {
+        console.log({ field });
 
-                };
+        const {
+          hidden = false,
+          fieldname = "",
+          label = "",
+          span = 24,
+          fieldtype,
+        } = field;
+        if (!hidden) {
+          const commonProps = {
+            // ...field,
+            label,
+            name: fieldname ?? "",
+            rules: getValidators(field),
+          };
 
-                return <Col xs={24} sm={24} md={span} key={fieldname}>
-                    {fieldtype !== 'File' ? <Form.Item {...commonProps} ><RenderField field={field} /></Form.Item>
-                        :
-                        <Form.Item
-                            {...commonProps}
-                            valuePropName="fileList"
-                            getValueFromEvent={(e) => Array.isArray(e) ? e : e?.fileList}
-                        >
-                            <Upload {...props}>
-                                {/* <Upload {...props} multiple> */}
-                                <Button icon={<UploadOutlined />}>Upload File</Button>
-                            </Upload>
-                        </Form.Item>
-                    }
-                </Col>
-            }
-        })
-    ), [schema]);
-
-    return (
-        <AppLoader isLoading={isLoading}>
-            <div className="table-header">
-                <h2 className="title">{title ?? ''}</h2>
-            </div>
-            <Form
-                name={`form_of_${screen}`}
-                form={form}
-                layout="vertical"
-                onFinish={(fields) => submit({ ...fields, fileList, id: MODE === KEY.EDIT ? initialData?.id : null }, redirect)}
-                initialValues={initialValues}
-            >
-                <Row gutter={16}>
-                    {fieldList}
-                </Row>
-
-                <Form.Item style={{ display: 'flex', justifyContent: 'end' }}>
-                    <Button type="primary" className="bg-primary" htmlType="submit">
-                        Submit
-                    </Button>
+          return (
+            <Col xs={24} sm={24} md={span} key={fieldname}>
+              {fieldtype !== "File" ? (
+                <Form.Item {...commonProps}>
+                  <RenderField field={field} />
                 </Form.Item>
-            </Form>
-        </AppLoader>
-    );
+              ) : (
+                <Form.Item
+                  {...commonProps}
+                  valuePropName="fileList"
+                  getValueFromEvent={(e) =>
+                    Array.isArray(e) ? e : e?.fileList
+                  }
+                >
+                  <Upload {...props}>
+                    {/* <Upload {...props} multiple> */}
+                    <Button icon={<UploadOutlined />}>Upload File</Button>
+                  </Upload>
+                </Form.Item>
+              )}
+            </Col>
+          );
+        }
+      }),
+    [schema]
+  );
+
+  return (
+    <AppLoader isLoading={isLoading}>
+      <div className="table-header">
+        <Title level={4}>{title ?? ""}</Title>
+      </div>
+      <Form
+        name={`form_of_${screen}`}
+        form={form}
+        layout="vertical"
+        onFinish={(fields) => submit({ ...fields, fileList }, redirect)}
+        initialValues={initialValues}
+      >
+        <Row gutter={16}>{fieldList}</Row>
+
+        <Form.Item style={{ display: "flex", justifyContent: "end" }}>
+          <Button htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </AppLoader>
+  );
 };
 
 export default FormBuilder;
--- src/layouts/ProtectedLayout.jsx
+++ src/layouts/ProtectedLayout.jsx
@@ -1,117 +1,132 @@
-import { useSelector } from 'react-redux';
-import { Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom';
-import { Input, Layout, Menu, Space } from 'antd';
+import { useSelector } from "react-redux";
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Input, Layout, Menu, Space, Typography } from "antd";
 import {
-    SettingOutlined,
-    BellOutlined,
-    SearchOutlined,
-} from '@ant-design/icons';
-import { ROUTES } from '../constants/routesConstants';
-import logo from '../assets/logo/CYDEA-GRC.png';
-import { isNotNullOrEmpty, isNullOrEmpty } from '../utils/utils';
-import { SIDE_MENU } from '../constants/menuConstants';
-import AvatarComp from '../components/Image/Avatar';
+  SettingOutlined,
+  BellOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
+import { ROUTES } from "../constants/routesConstants";
+import logo from "../assets/logo/CYDEA-GRC.png";
+import { isNotNullOrEmpty, isNullOrEmpty } from "../utils/utils";
+import { SIDE_MENU } from "../constants/menuConstants";
+import AvatarComp from "../components/Image/Avatar";
 
 const { Header, Sider, Content } = Layout;
+const { Title } = Typography;
 
 const ProtectedLayout = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const user = useSelector((state) => state.session.user);
-    const route = SIDE_MENU.find(item => item?.key === location.pathname);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const user = useSelector((state) => state.session.user);
+  const route = SIDE_MENU.find((item) => item?.key === location.pathname);
 
-    if (isNullOrEmpty(user)) {
-        return <Navigate to={ROUTES.PUBLIC.ROOT} replace />;
-    }
+  if (isNullOrEmpty(user)) {
+    return <Navigate to={ROUTES.PUBLIC.ROOT} replace />;
+  }
 
-    return (
-        <Layout style={{ minHeight: '100vh', background: '#F5F7FA' }}>
-            {/* Sidebar */}
-            <Sider
-                width={220}
-                style={{ background: '#fff', borderRight: '1px solid #f0f0f0' }}
+  return (
+    <Layout style={{ minHeight: "100vh", background: "#F5F7FA" }}>
+      {/* Sidebar */}
+      <Sider
+        width={220}
+        style={{ background: "#fff", borderRight: "1px solid #f0f0f0" }}
+      >
+        <Space direction="vertical" size="large">
+          <div className="flex justify-center items-center py-4">
+            <img src={logo} alt="Logo" className="w-32" />
+          </div>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={[ROUTES.PRIVATE.ROOT]}
+            selectedKeys={[window.location.pathname]}
+            style={{ height: "100%", borderRight: 0 }}
+            items={SIDE_MENU.map((item) => {
+              let newItem = {
+                ...item,
+                style: { marginBottom: 12 },
+                onClick: () => navigate(item?.key),
+              };
+              if (isNotNullOrEmpty(item?.children)) {
+                delete newItem.onClick;
+                // newItem.onClick = () => navigate(item.children[0]?.key)
+                newItem.children = item.children.map((child) => ({
+                  ...child,
+                  style: { marginBottom: 12 },
+                  onClick: () => navigate(child?.key),
+                }));
+              }
+              return newItem;
+            })}
+          />
+        </Space>
+      </Sider>
+
+      <Layout>
+        <Header
+          style={{
+            background: "#fff",
+            padding: "0 24px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            borderBottom: "1px solid #f0f0f0",
+          }}
+        >
+          <Title level={4}>{route?.label ?? ""}</Title>
+          <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+            <Input
+              size="large"
+              placeholder="Search for something"
+              prefix={<SearchOutlined style={{ fontSize: "20px" }} />}
+              style={{
+                borderRadius: 40,
+                background: "#F5F7FA",
+                color: "#8BA3CB",
+                padding: "10px",
+                gap: 10,
+              }}
+            />
+
+            <div
+              className="flex items-center gap-4"
+              style={{ display: "flex", alignItems: "center", gap: "20px" }}
             >
-                <Space direction='vertical' size='large'>
-                    <div className="flex justify-center items-center py-4">
-                        <img src={logo} alt="Logo" className="w-32" />
-                    </div>
-                    <Menu
-                        mode="inline"
-                        defaultSelectedKeys={[ROUTES.PRIVATE.ROOT]}
-                        selectedKeys={[window.location.pathname]}
-                        style={{ height: '100%', borderRight: 0 }}
-                        items={SIDE_MENU.map(item => {
-                            let newItem = {
-                                ...item,
-                                style: { marginBottom: 12 },
-                                onClick: () => navigate(item?.key)
-                            }
-                            if (isNotNullOrEmpty(item?.children)) {
-                                delete newItem.onClick
-                                // newItem.onClick = () => navigate(item.children[0]?.key)
-                                newItem.children = item.children.map(child => ({
-                                    ...child,
-                                    style: { marginBottom: 12 },
-                                    onClick: () => navigate(child?.key)
-                                }))
-                            }
-                            return newItem
-                        })}
-                    />
-                </Space>
-            </Sider>
+              <span
+                className="text-gray-500"
+                style={{
+                  borderRadius: 50,
+                  lineHeight: 0,
+                  background: "#F5F7FA",
+                  color: "#8BA3CB",
+                  padding: "10px",
+                }}
+              >
+                <SettingOutlined style={{ fontSize: "24px" }} />
+              </span>
+              <span
+                className="text-gray-500"
+                style={{
+                  borderRadius: 50,
+                  lineHeight: 0,
+                  background: "#F5F7FA",
+                  color: "#8BA3CB",
+                  padding: "10px",
+                }}
+              >
+                <BellOutlined style={{ fontSize: "24px", color: "#FE5C73" }} />
+              </span>
+              <AvatarComp />
+            </div>
+          </div>
+        </Header>
 
-            <Layout>
-                <Header
-                    style={{
-                        background: '#fff',
-                        padding: '0 24px',
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        borderBottom: '1px solid #f0f0f0',
-                    }}
-                >
-                    <h2 style={{ fontSize: 18, fontWeight: 600 }}>{route?.label ?? ''}</h2>
-                    <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
-                        <Input size="large" placeholder="Search for something" prefix={<SearchOutlined style={{ fontSize: '20px' }} />} style={{
-                            borderRadius: 40,
-                            background: '#F5F7FA',
-                            color: '#8BA3CB',
-                            padding: '10px',
-                            gap: 10
-                        }} />
-
-                        <div className="flex items-center gap-4" style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
-                            <span className="text-gray-500" style={{
-                                borderRadius: 50,
-                                lineHeight: 0,
-                                background: '#F5F7FA',
-                                color: '#8BA3CB',
-                                padding: '10px'
-                            }} >
-                                <SettingOutlined style={{ fontSize: '24px' }} />
-                            </span>
-                            <span className="text-gray-500" style={{
-                                borderRadius: 50,
-                                lineHeight: 0,
-                                background: '#F5F7FA',
-                                color: '#8BA3CB',
-                                padding: '10px'
-                            }} >
-                                <BellOutlined style={{ fontSize: '24px', color: '#FE5C73' }} />
-                            </span>
-                            <AvatarComp />
-                        </div>
-                    </div>
-                </Header>
-
-                <Content style={{ margin: '24px', padding: 24, background: '#f9f9f9' }}>
-                    <Outlet />
-                </Content>
-            </Layout>
-        </Layout >
-    );
+        <Content style={{ margin: "24px", padding: 24, background: "#f9f9f9" }}>
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
 };
 
 export default ProtectedLayout;
