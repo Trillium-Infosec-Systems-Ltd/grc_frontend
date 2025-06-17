fe5beca70540e11ae5bc5dd6adb7e311fde4d29e
unknown <muttrafgujjar.787@gmail.com>
2025-06-17 22:17:52 +0500

    Add question table field case to render in form

--- src/components/Field/FieldRender.jsx
+++ src/components/Field/FieldRender.jsx
@@ -1,44 +1,67 @@
-import { Form, Input, Select, DatePicker, Upload, Button, Radio, Typography } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import {
+  Form,
+  Input,
+  Select,
+  DatePicker,
+  Upload,
+  Button,
+  Radio,
+  Typography,
+  InputNumber,
+} from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 import { getValidators } from "../Form/validator";
-import GenericSelect from './GenericSelect'
-import { useMemo } from 'react';
+import GenericSelect from "./GenericSelect";
+import { useMemo } from "react";
+import QuestionTable from "./QuestionTable";
 
 const { TextArea } = Input;
 const { Text } = Typography;
 
 const RenderField = ({ field = {}, ...rest }) => {
-    const formField = useMemo(() => {
-        switch (field?.fieldtype) {
-            case 'Data':
-                return <Input {...rest} />;
-            case 'Select':
-                return <GenericSelect {...rest} field={field} />;
-            case 'Radio':
-                return (
-                    <Radio.Group {...rest} options={field?.options?.map(opt => ({ label: opt ?? '', value: opt ?? '' }))} />
-                );
-            case 'LongText':
-                return <TextArea {...rest} rows={4} />;
-            case 'Date':
-                return <DatePicker {...rest} style={{ width: '100%' }} />;
-            case 'Tags':
-                return <Select {...rest} mode="tags" />;
-            case 'MultiLink':
-            case 'MultiSelect':
-                return <GenericSelect {...rest} field={field} mode="multiple" />;
-            case 'Link':
-                return <GenericSelect {...rest} field={field} />;
-            case 'Text':
-                return <Text style={{ maxWidth: '100%' }} {...rest} field={field}>
-                    {field?.default_value ?? ''}
-                </Text>
-            default:
-                return null;
-        }
-    }, [field, rest])
+  const formField = useMemo(() => {
+    switch (field?.fieldtype) {
+      case "Data":
+        return <Input {...rest} />;
+      case "Float":
+        return <InputNumber {...rest} min={field.min} max={field.max} />;
+      case "Select":
+        return <GenericSelect {...rest} field={field} />;
+      case "Radio":
+        return (
+          <Radio.Group
+            {...rest}
+            options={field?.options?.map((opt) => ({
+              label: opt ?? "",
+              value: opt ?? "",
+            }))}
+          />
+        );
+      case "LongText":
+        return <TextArea {...rest} rows={4} />;
+      case "Date":
+        return <DatePicker {...rest} style={{ width: "100%" }} />;
+      case "Tags":
+        return <Select {...rest} mode="tags" />;
+      case "MultiLink":
+      case "MultiSelect":
+        return <GenericSelect {...rest} field={field} mode="multiple" />;
+      case "Link":
+        return <GenericSelect {...rest} field={field} />;
+      case "question_table":
+        return <QuestionTable {...rest} field={field} />;
+      case "Text":
+        return (
+          <Text style={{ maxWidth: "100%" }} {...rest} field={field}>
+            {field?.default_value ?? ""}
+          </Text>
+        );
+      default:
+        return null;
+    }
+  }, [field, rest]);
 
-    return formField
+  return formField;
 };
 
-export default RenderField
+export default RenderField;
