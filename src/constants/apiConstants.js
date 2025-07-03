dfb04d998381e890ab0d14c079a63c5a6b94020f
unknown <muttrafgujjar.787@gmail.com>
2025-07-04 02:21:16 +0500

    Implemented to fetch data on the base of field value

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,53 +1,74 @@
 export const API_CONTENT_TYPE = {
-  MULTIPART: 'multipart/form-data',
+  MULTIPART: "multipart/form-data",
 };
 
 export const API_METHOD = {
-  GET: 'GET',
-  POST: 'POST',
-  PUT: 'PUT',
+  GET: "GET",
+  POST: "POST",
+  PUT: "PUT",
 };
 
 export const APIS = {
   TABLE_SCHEMA: {
-    URL: '/table_meta/',
+    URL: "/table_meta/",
     METHOD: API_METHOD.GET,
     AUTH: false,
   },
   FORM_SCHEMA: {
-    URL: '/schemas/',
+    URL: "/schemas/",
     METHOD: API_METHOD.GET,
     AUTH: false,
   },
   GET_RECORDS: {
-    URL: '/data/',
+    URL: "/data/",
     METHOD: API_METHOD.GET,
     AUTH: false,
   },
   CREATE_RECORD: {
-    URL: '/data/',
+    URL: "/data/",
     METHOD: API_METHOD.POST,
     AUTH: false,
-    PAYLOAD: {}
+    PAYLOAD: {},
   },
   UPDATE_RECORD: {
-    URL: '/data/',
+    URL: "/data/",
     METHOD: API_METHOD.PUT,
     AUTH: false,
-    PAYLOAD: {}
+    PAYLOAD: {},
   },
-  LINK_OPTIONS: {
-    URL: '/link-options',
+  ASSETS_INFO: {
+    URL: "/assets_info/",
+    METHOD: API_METHOD.GET,
+    AUTH: false,
+  },
+  THREAT_INFO: {
+    URL: "/threat_info/",
     METHOD: API_METHOD.GET,
     AUTH: false,
     PARAMS: {
-      QUERY: { document_type: '', field: '', search_term: '', offset: 0 },
-    }
+      QUERY: { asset_value: "" },
+    },
+  },
+  LINK_OPTIONS: {
+    URL: "/link-options",
+    METHOD: API_METHOD.GET,
+    AUTH: false,
+    PARAMS: {
+      QUERY: { document_type: "", field: "", search_term: "", offset: 0 },
+    },
   },
   UPLOAD: {
-    URL: '/upload_files',
+    URL: "/upload_files",
     METHOD: API_METHOD.POST,
     CONTENT_TYPE: API_CONTENT_TYPE.MULTIPART,
     AUTH: false,
+  },
+  DYNAMIC_CALL: {
+    URL: "",
+    METHOD: API_METHOD.GET,
+    AUTH: false,
+    PARAMS: {
+      QUERY: {},
+    },
   },
 };
