44d3ff1e335512eaccb2695e43bbaf1ac65facc2
App-Developer8866 <muttrafgujjar.787@gmail.com>
2025-07-30 23:10:25 +0500

    Refactor ThreatForm component for improved code readability and formatting

--- src/pages/ThreatsHub/pages/ThreatForm.jsx
+++ src/pages/ThreatsHub/pages/ThreatForm.jsx
@@ -1,12 +1,15 @@
-import FormBuilder from '../../../components/Form/FormBuilder';
-import { KEY } from '../../../constants/keysConstants';
-import { ROUTES } from '../../../constants/routesConstants';
-
+import FormBuilder from "../../../components/Form/FormBuilder";
+import { KEY } from "../../../constants/keysConstants";
+import { ROUTES } from "../../../constants/routesConstants";
 
 const ThreatForm = ({ MODE = KEY.VIEW }) => {
-
   return (
-    <FormBuilder screen='threat' title={MODE === KEY.EDIT ? 'Edit Threat' : 'Add New Threat'} redirect={ROUTES.PRIVATE.THREATS_HUB.PARENT} MODE={MODE} />
+    <FormBuilder
+      screen="threat"
+      title={MODE === KEY.EDIT ? "Edit Threat" : "Add New Threat"}
+      redirect={ROUTES.PRIVATE.THREATS_HUB.PARENT}
+      MODE={MODE}
+    />
   );
 };
 
